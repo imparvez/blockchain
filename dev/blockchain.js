27543469030b46268220b400c74e610539b49d62
Parvez Shaikh <parvezshaikhj@gmail.com>
2018-09-16 21:47:16 +0530

    New block creation, hash value generation, nonce value calculation

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,9 +1,14 @@
+const sha256 = require('sha256')
+
 /**
  * Blockchain constructor function will help in creating data structure
  */
 function Blockchain(){
 	this.chain = []; // It will contain all the blocks
-	this.pendingTransactions = []; // It will contain all the records of new transaction before being pushed to `this.chain`
+	this.pendingTransactions = []; // It will contain all the records of new transactions before being pushed to `this.chain` a.k.a in to the block
+
+	// Creating the Genesis Block -> The first block in the block chain
+	this.createNewBlock(100, '0', '0');
 }
 
 /**
@@ -13,7 +18,7 @@ function Blockchain(){
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
 	// Creating a new block
 	const newBlock = { // all the important piece of data will be stored here
-		index: this.chain.length + 1, // Number of block
+		index: this.chain.length + 1, // Number of block(1,2,3...)
 		timestamp: Date.now(), // when this block is created 
 		transactions: this.pendingTransactions,
 		nonce: nonce, // could be any number. It'll be a proof which indicates that we have created this block in legitimate way(Proof of work)
@@ -21,7 +26,8 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
 		previousBlockHash: previousBlockHash // hash of previous block
 	}
 
-	// clearing out the new transactions
+	// clearing out the pending transactions array so that new transaction can take place
+	// to build up new block in the chain
 	this.pendingTransactions = [];
 
 	// pushing all the changes to `this.chain`
@@ -50,6 +56,38 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
 	this.pendingTransactions.push(newTransaction);
 
 	return this.getLastBlock()['index'] + 1;
+}
+
+/*
+* Creating hash block function that will take a block as an input and return a hash value string
+*/
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentData, nonce){
+	const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentData);
+	const hash = sha256(dataAsString);
+	return hash;
+}
+
+/*
+* Proof of work
+*/
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentData){
+	/*
+	* => repeatedly hashBlock function, until it finds the correct hash => '0000GIUC8SD7V98ADYV98' => starting with 0000
+	* => uses previousBlockHash for the hash, also currentData
+	* => continously changes nonce to get the correct value of hash
+	* => returns the nonce value that creates the correct hash
+	*/
+
+	let nonce = 0;
+	let hash = this.hashBlock(previousBlockHash, currentData, nonce);
+
+	while(hash.substring(0,4) !== '0000'){
+		nonce++;
+		hash= this.hashBlock(previousBlockHash, currentData, nonce)
+		console.log(hash)
+	}
+
+	return nonce; // will return the number of iteration to generate the correct hash
 }
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,13 +1,54 @@
 const Blockchain = require('./blockchain');
 
-console.log(Blockchain);
-
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(1234, 'previousblockhash', 'currentHash');
+/*
+* Test Case: 1
+*/
 
-bitcoin.createNewTransaction(100, 'senderAddress', 'recipientAddress');
+// bitcoin.createNewBlock(1234, 'previousblockhash', 'currentHash');
 
-bitcoin.createNewBlock(5678, 'newPreviousHash', 'newCurrentHash');
+// bitcoin.createNewTransaction(100, 'senderAddress', 'recipientAddress');
 
-console.log(bitcoin.chain[1]);
+// bitcoin.createNewBlock(5678, 'newPreviousHash', 'newCurrentHash');
+
+// bitcoin.createNewTransaction(50, 'MUMBAI', 'NEWYORK');
+// bitcoin.createNewTransaction(300, 'NEWYORK', 'DHAKA');
+// bitcoin.createNewTransaction(2000, 'DHAKA', 'SRILANKA');
+
+// bitcoin.createNewBlock(9619442263, '09AHIGHT', 'E0945687');
+
+// console.log(bitcoin);
+/*
+* Test Case: 2
+*/
+const previousBlockHash = '09SDFU987SD8OCNDS89'
+const nonce = 1000
+const currentData = [
+    {
+        amount: 100,
+        sender: 'BOMBAY',
+        recepient: 'NEWYORK'
+    },{
+        amount: 1000,
+        sender: 'BANDRA',
+        recepient: 'THANE'
+    },{
+        amount: 5,
+        sender: 'BANDRA(W)',
+        recepient: 'BANDRA(E)'
+    },
+];
+
+// console.log(bitcoin.hashBlock(previousBlockHash, currentData, nonce));
+
+/*
+* Test Case: 3 Calculating Nonce Value using proof of work
+*/
+// console.log(bitcoin.proofOfWork(previousBlockHash, currentData));
+// console.log(bitcoin.hashBlock(previousBlockHash, currentData, 13009));
+
+/*
+* Test Case: 4 Testing the genesis block
+*/
+console.log(bitcoin);
